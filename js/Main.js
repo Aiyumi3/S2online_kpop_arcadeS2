2842b60ba53d915a2614a6989f58fc37dea41aaf
Aiyumi Sunshine <62023811+Aiyumi3@users.noreply.github.com>
2022-03-30 09:57:24 +0300

    add

--- js/Main.js
+++ js/Main.js
@@ -355,13 +355,6 @@ class KPopGame extends Phaser.Scene {
             this.setScale(0.017);
             this.setVisible(false);
         });
-	let timer = setInterval(function() {
-	  console.log(time++);
-	   if(time == 180){
-	       heal.setVisible(true); 
-	   }
-        }, 1000);
-	clearInterval(timer);
        
 	progress = this.add.graphics().setScrollFactor(0); //is fixed to camera;
         this.add.image(591, 268, 'watermelon').setScale(0.011).setScrollFactor(0); //is fixed to camera
@@ -506,6 +499,13 @@ class KPopGame extends Phaser.Scene {
         if(gameOver){
             return;  //allows to show alert
         }
+	let timer = setInterval(function() {
+	  console.log(time++);
+	   if(time == 180){
+	       heal.setVisible(true); 
+	       clearInterval(timer); 
+	   }
+        }, 1000);
 	    
 	if(score == 700){
             heal.setVisible(true);
