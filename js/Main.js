2490abadeccd608f47aed55409f42c8dfc83799f
Aiyumi Sunshine <62023811+Aiyumi3@users.noreply.github.com>
2022-04-03 23:20:53 +0300

    ch

--- js/Main.js
+++ js/Main.js
@@ -556,18 +556,20 @@ class KPopGame extends Phaser.Scene {
             winnerMg.setScrollFactor(0); // is fixed to cam
             winnerMg.setScale(0.29);  //smaller
             winnerMg.setDepth(1); //on top
-	    let scoreTxt = this.add.text(500, 320, '', { fontSize: '11pt', fill: '#ffffff', fontFamily:'Comic Sans MS'});
+		
+	    let scoreTxt = this.add.text(500, 350, ` ${score}`, { fontSize: '11pt', fill: '#ffffff', fontFamily:'Comic Sans MS'});
+	    scoreTxt.setScrollFactor(0); //is fixed to camera
 	    scoreTxt.setDepth(1);
-	    let heartTxt = this.add.text(500, 350, '', { fontSize: '11pt', fill: '#ffffff', fontFamily:'Comic Sans MS'});
+	    let heartTxt = this.add.text(500, 370, ` ${hearts}`, { fontSize: '11pt', fill: '#ffffff', fontFamily:'Comic Sans MS'});
+	    heartTxt.setScrollFactor(0); //is fixed to camera
             heartTxt.setDepth(1);
-	    scoreTxt.setText(` ${score}`);
-            heartTxt.setText(` ${hearts}`);
-
-            let continueText = this.add.text(500, 400, 'continue', { fontSize: '7pt', fill: '#ffffff', fontFamily:'Comic Sans MS'});
+	    //scoreTxt.setText(` ${score}`);
+            //heartTxt.setText(` ${hearts}`);
+            let continueText = this.add.text(550, 430, 'continue', { fontSize: '7pt', fill: '#ffffff', fontFamily:'Comic Sans MS'});
             continueText.setScrollFactor(0); //is fixed to camera
 	    continueText.setInteractive();
 	    continueText.setDepth(1);
-            let reloadText = this.add.text(570, 400, 'reload', { fontSize: '7pt', fill: '#ffffff', fontFamily:'Comic Sans MS'});
+            let reloadText = this.add.text(620, 430, 'reload', { fontSize: '7pt', fill: '#ffffff', fontFamily:'Comic Sans MS'});
             reloadText.setScrollFactor(0); //is fixed to camera
             reloadText.setInteractive();
 	    reloadText.setDepth(1);
